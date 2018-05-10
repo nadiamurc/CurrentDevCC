9f29c7c52fb6e3f766409bda452c97e8044a27fb
aidan <aidancrum@gmail.com>
2018-05-10 16:14:56 +0100

    erer test

---
+++ CC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CC.Startup))]
+namespace CC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
